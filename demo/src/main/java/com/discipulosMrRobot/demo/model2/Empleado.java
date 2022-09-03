5121da0df128b72b76122e8747386d16a5d38e79
Mariana Contreras <marianac532@gmail.com>
2022-09-03 17:47:12 -0500

    Prueba solo Empresa

--- demo/src/main/java/com/discipulosMrRobot/demo/model2/Empleado.java
+++ demo/src/main/java/com/discipulosMrRobot/demo/model2/Empleado.java
@@ -11,17 +11,23 @@ import javax.persistence.Column;
 
 @Data
 @Entity
-@Table(name = "MovimientoDinero")
-public class MovimientoDinero {
+@Table(name = "Empleado")
+public class Empleado {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idMovimiento;
+    private int idEmpleado;
 
-    @Column(length = 20,nullable = false)
-    private int monto;
+    @Column(length = 60,nullable = false)
+    private String nombre;
 
     @Column(length = 50,nullable = false)
-    private String concepto;
+    private String correo;
+
+    @Column(length = 50,nullable = false)
+    private String empresa;
+
+    @Column(length = 50,nullable = false)
+    private String rol;
 
 }
--- demo/src/main/java/com/discipulosMrRobot/demo/model2/Empresa.java
+++ demo/src/main/java/com/discipulosMrRobot/demo/model2/Empresa.java
@@ -2,19 +2,31 @@ package com.discipulosMrRobot.demo.model2;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 
 @Data
+@Entity
+@Table(name = "Empresa")
+public class Empresa {
 
-public class Empresa2 {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idEmpresa;
+
     @Column(length = 50,nullable = false)
     private String nombreEmpresa;
+
     @Column(length = 30,nullable = false)
     private String direccion;
+
     @Column(length = 20,nullable = false)
     private String telefono;
+
     @Column(length = 20,nullable = false)
     private String nit;
 
